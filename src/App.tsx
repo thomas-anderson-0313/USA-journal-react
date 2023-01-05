7ab0a13ac4b185fd2a3e893281934d2ea6a3f29b
thomas-anderson-0313 <thomasanderson19960313@gmail.com>
2023-01-06 09:26:37 +1000

    fixed_some_design

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ function App() {
             <div className='main-descrition'>
               {/* <div className='main-des-title'>Biden Extends Free Health Subsidies For Americans Making Less Than $50k/year</div> */}
               <img src = {Head_img} alt = "head" width = "100%" />
-              <div className='mian-des-1'>The deadline to lock in your health subsidy ends on January 15th, so call the hotline if you qualify!</div>
+              <div className='mian-des-1'>The deadline to lock in your health subsidy ends on <span style = {{fontWeight:"700"}}>January 15th</span>, so call the hotline if you qualify!</div>
               <div className='main-des-2'>Answer 2 simple questions below to check eligibility in just 30 seconds</div>
             </div>
             <div className='survey'>
